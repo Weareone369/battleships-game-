b9d0cb930ba0cdc0694b08bfe16c0c3859985c1a
Olga Molinaro <sophia_ballare@hotmail.co.uk>
2024-11-14 19:02:08 +0000

    Added method for placing ships on the grid with validation.

--- src/grid.py
+++ src/grid.py
@@ -7,3 +7,14 @@ def __init__(self, size):
     self.size = size 
     self.ships = [] 
     self.grid = [['~' for _ in range(size)] for _ in range(size)]
+
+def add_ship(self, ship): 
+    """ Add a ship to the grid if placement is valid. A ship is defined as a list of (x, y) tuples representing its coordinates. """ 
+    for x, y in ship: 
+        if not (0 <= x < self.size and 0 <= y < self.size): 
+            raise ValueError("Ship is out of bounds.") 
+        if self.grid[x][y] != '~': 
+            raise ValueError("Ship overlaps with another ship.") 
+            
+    self.ships.append(ship) 
+    for x, y in ship: self.grid[x][y] = 'S' 
