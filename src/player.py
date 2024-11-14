6bd48c1a360a5a0ad84823779e06f674b7b230ca
Olga Molinaro <sophia_ballare@hotmail.co.uk>
2024-11-14 19:10:14 +0000

    Initialized Player class with name and grid.

---
+++ src/player.py
@@ -0,0 +1,7 @@
+class Player:
+     """Represents a player in the game, managing their grid and shots.""" 
+     
+     def __init__(self, name, grid): 
+        """Initialize player with a name and their own grid.""" 
+        self.name = name 
+        self.grid = grid 
